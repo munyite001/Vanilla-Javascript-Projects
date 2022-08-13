556d3c5f5df9dd475ab691b917479a9c06feda54
munyite001 <emunyite@gmail.com>
2022-08-13 13:20:39 +0300

    Video project with pre loader

---
+++ Video Project/app.js
@@ -0,0 +1,28 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+//  Grab the preloader
+const preloader = document.querySelector('.preloader');
+
+//  Once the window has loaded, we hide the preloader
+window.addEventListener('load',()=> {
+    preloader.classList.add('hide-preloader')
+})
+
+//  Button event Listener
+btn .addEventListener('click',()=>{
+    if(!btn.classList.contains('slide'))
+    {
+        btn.classList.add('slide');
+        video.pause();
+    }
+    else
+    {
+        btn.classList.remove('slide');
+        video.play();
+    }
+})
